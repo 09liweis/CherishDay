eeee0d7b9e83e71b29fbe6cf9fa260932ce6dfd1
Siri Yeung <491763280@qq.com>
2025-07-20 13:54:50 +0000

    add database collection env variable

--- app.config.js
+++ app.config.js
@@ -7,6 +7,9 @@ export default ({ config }) => {
       ...config.extra,
       appwriteEndpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
       appwriteProjectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+      appwriteDatabaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+      appwriteCollectionId: process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID,
+      appwriteBucketId: process.env.EXPO_PUBLIC_APPWRITE_BUCKET_ID,
     },
   };
 };
