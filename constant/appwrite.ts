f27b2f4952d898ef4f8f64196ce9f4361af30ffa
09liweis <weisen.li@hotmail.com>
2025-07-21 14:23:03 -0400

    remove appwrite platform

--- constant/appwrite.ts
+++ constant/appwrite.ts
@@ -16,7 +16,7 @@ export const client = new Client();
 client
   .setEndpoint(APPWRITE_ENDPOINT) // 设置 Appwrite 端点
   .setProject(APPWRITE_PROJECT_ID) // 设置项目 ID
-  .setPlatform('com.cherishdays.app'); 
+  ; 
 
 // 导出 Appwrite 配置，以便在其他地方使用
 export const APPWRITE_CONFIG = {
