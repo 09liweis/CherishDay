a1f405d764a1c433bba8f8a7b43933301e0e1d41
09liweis <weisen.li@hotmail.com>
2025-07-25 09:45:44 -0400

    add friends icon

--- constant/icons.ts
+++ constant/icons.ts
@@ -1,5 +1,5 @@
-import { Calendar, User, Plus, CircleAlert as AlertCircle, X, Clock, RotateCcw, Trash2, Cake, ChevronDown, ArrowLeft, TrendingUp, LogOut, LayoutGrid, List } from "lucide-react-native";
+import { Calendar, User, Plus, CircleAlert as AlertCircle, X, Clock, RotateCcw, Trash2, Cake, ChevronDown, ArrowLeft, TrendingUp, LogOut, LayoutGrid, List, Users, Search, UserPlus, MessageCircle, Mail, Lock, Eye, EyeOff } from "lucide-react-native";
 
 export {
-  Calendar, User, Plus, AlertCircle, X, Clock, RotateCcw, Trash2, Cake, ChevronDown, ArrowLeft, TrendingUp, LogOut, LayoutGrid, List
+  Calendar, User, Plus, AlertCircle, X, Clock, RotateCcw, Trash2, Cake, ChevronDown, ArrowLeft, TrendingUp, LogOut, LayoutGrid, List, Users, Search, UserPlus, MessageCircle, Mail, Lock, Eye, EyeOff
 };
